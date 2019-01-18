f3ce57d3ed61fb391d0d7910886ce522dc66141a
Billy Witherspoon <wwither@gmail.com>
2019-01-18 05:50:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,10 @@
 def line(katz_deli)
   if katz_deli.length > 0
-    puts katz_deli
+    numberedLine = []
+    katz_deli.each_with_index do |name, i|
+      numberedLine >> " #{i + 1}. #{name}"
+    end
+    return "The line is currently:${numberedLine}"
   else
     puts "The line is currently empty."
   end
