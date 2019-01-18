de73cf9db6ad7d477257aabcff59711d53483f73
Billy Witherspoon <wwither@gmail.com>
2019-01-18 06:01:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  "Welcome #{name} you are number #{katz_deli.lenght} in line"
+end
