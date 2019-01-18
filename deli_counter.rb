23a97a0ad96d24f63cf9988298da214af14a74ce
Billy Witherspoon <wwither@gmail.com>
2019-01-18 05:52:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   if katz_deli.length > 0
     numberedLine = []
     katz_deli.each_with_index do |name, i|
-      numberedLine >> " #{i + 1}. #{name}"
+      numberedLine << " #{i + 1}. #{name}"
     end
     return "The line is currently:#{numberedLine}"
   else
