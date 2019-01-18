0a4f72ff2694a6de736c0ce5b939ecbf4b0a6e76
Billy Witherspoon <wwither@gmail.com>
2019-01-18 06:06:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  katz_deli.length > 0 ? puts "Currently serving #{katz_deli.shift}." : puts "There is nobody waiting to be served."
+  if katz_deli.length > 0
+    puts "Currently serving #{katz_deli.shift}."
+  else
+    puts "There is nobody waiting to be served."
+  end
 end
